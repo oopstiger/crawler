b1190327a5f7b03c23e87a699c45c9baf4d8863c
algoriz <riz.hsu@gmail.com>
2015-03-06 10:27:42 +0800

    merge dianping and meituan crawler

--- dianping/fetch.py
+++ dianping/fetch.py
@@ -1,4 +1,51 @@
+import os
+import sys
+import urllib2
+import logging
+import datetime
+from bs4 import BeautifulSoup
+
+sys.path.append(os.path.abspath("../"))
+from ReviewRecord import *
+
 
 def fetch(args):
-    pass
+    hotel_name = args[0]
+    hotel_id = args[1]
+    api_url = hotel_id
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20141201 Firefox/3.5.6'}
+    req = urllib2.Request(url=api_url, headers=headers)
+    f = urllib2.urlopen(req)
+    if f.getcode() != 200:
+        logging.error("http %d --> Get %s " % (f.getcode(), api_url))
+        return None
 
+    doc = f.read()
+    soup = BeautifulSoup(doc)
+    recos = soup.find_all('li', attrs = {"class": "comment-item"})
+
+    r1 = []
+    for reco in recos:
+        r = ReviewRecord()
+        r.hotel_name = hotel_name
+        r.hotel_url = api_url
+        r.source_site = "dianping"
+        r.nick_name = reco.find('a', class_='name').string
+
+        comment_date = reco.find('span', class_= 'time').string
+        if str(comment_date).split("-")[0] != '14':
+            r.comment_date = '15-' + comment_date
+        else:
+            r.comment_date = comment_date
+        r.check_in_date = r.comment_date
+        comment = reco.find('p', class_ = 'desc J-desc')
+        if comment == None:
+            r.comment = reco.find('p', class_ = 'desc').string
+        else:
+            r.comment = comment.string
+        r.consume_detail = ""
+        rate = reco.find('p', class_ = 'shop-info').find('span')
+        r.rate = int(str(rate).split("str")[1].split("0")[0])
+        r.timestamp = datetime.datetime.now()
+        r1.append(r)
+    return r1
--- meituan/fetch.py
+++ meituan/fetch.py
@@ -1,3 +1,42 @@
+import os
+import sys
+import urllib
+import logging
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+sys.path.append(os.path.abspath("../"))
+from ReviewRecord import *
+
 
 def fetch(args):
-    pass
+    hotel_name = args[0]
+    hotel_url = args[1]
+    page = urllib.urlopen(hotel_url)
+    if page.getcode() != 200:
+        logging.error("HTTP %d --> GET %s " % (f.getcode(), api_url))
+        return None
+    html_doc = page.read()
+    TotalScore = 5
+    soup = BeautifulSoup(html_doc)
+    recos = soup.find_all('li', attrs={"class": "J-ratelist-item rate-list__item"})
+    # hotel_name = soup.find('div', class_ = 'hotel-title').find('h5').string
+    hotel_url = soup.find('link', rel='canonical')['href']
+
+    rl = []
+    for reco in recos:
+        r = ReviewRecord()
+        r.source_site = 'meituan'
+        r.timestamp = datetime.now()
+        r.hotel_name = hotel_name
+        r.hotel_url = hotel_url
+        r.nick_name = reco.find('span', class_='name').string
+        r.comment_date = reco.find('span', class_='time').string
+        r.check_in_date = r.comment_date
+        r.comment = reco.find('p', class_='content').string
+        #print r.comment.encode('utf-8')
+        r.consume_detail = reco.find('p', class_='deal-title').find('a').string
+        r.rate = int(reco.find(style=True)['style'].split(':')[1].split('%')[0]) * TotalScore / 100
+        rl.append(r)
+    return rl
